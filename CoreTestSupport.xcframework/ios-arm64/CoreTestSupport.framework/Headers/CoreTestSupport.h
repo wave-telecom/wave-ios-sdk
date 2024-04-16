58ebf59a1cdd2cdf238addcbea07939f29c7c6af
Victor <joaovictor_mendes@hotmail.com>
2024-04-16 11:17:01 -0300

    feat: add more packages

---
+++ CoreTestSupport.xcframework/ios-arm64/CoreTestSupport.framework/Headers/CoreTestSupport.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreTestSupport.
+FOUNDATION_EXPORT double CoreTestSupportVersionNumber;
+
+//! Project version string for CoreTestSupport.
+FOUNDATION_EXPORT const unsigned char CoreTestSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
